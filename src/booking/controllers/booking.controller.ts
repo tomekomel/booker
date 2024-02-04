6f6c79d52beabdaa76325d74a5451503d38bbf2f
Tomasz Omeljaniuk <tomekomel@gmail.com>
2024-02-05 00:01:41 +0100

    feat: Create migration creating user and admin

--- src/booking/controllers/booking.controller.ts
+++ src/booking/controllers/booking.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, NotFoundException } from '@nestjs/common';
 import { BookingService } from '../services/booking.service';
+import { BookingEntity } from '../entities';
 
 @Controller('bookings')
 export class BookingController {
   constructor(private readonly bookingService: BookingService) {}
 
   @Get(':bookingId')
-  async getBooking(bookingId: number) {
+  async getBooking(bookingId: number): Promise<BookingEntity> {
     if (!(await this.bookingService.exists(bookingId))) {
       throw new NotFoundException('Booking not found!');
     }
---
+++ src/migrations/1707086879349-CreateAdminAndUser.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAdminAndUser1707086879349 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into('user')
+      .values([
+        {
+          id: null,
+          firstName: 'Tom',
+          lastName: 'Book',
+          email: 'tom@book.com',
+          role: 'user',
+          token:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxIiwicm9sZSI6InVzZXIiLCJpYXQiOjE1MTYyMzkwMjJ9.8aOP6TSAYKGphtUf7LCG0uQcgERrDAjTCbfOJrnV290',
+        },
+        {
+          id: null,
+          firstName: 'Jeff',
+          lastName: 'Boss',
+          email: 'jeff@book.com',
+          role: 'admin',
+          token:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTE2MjM5MDIyfQ.5iVcEnlVHQS72AFmMY9cln5A5eQTvPWaNC94Lbx8-8M',
+        },
+      ])
+      .execute();
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
