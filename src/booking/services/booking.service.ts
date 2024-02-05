6bcbd476afacb538871e2b288fbb33385344124e
Tomasz Omeljaniuk <tomekomel@gmail.com>
2024-02-05 22:50:18 +0100

    feat: Add start and end time validation

--- src/booking/services/booking.service.ts
+++ src/booking/services/booking.service.ts
@@ -1,7 +1,12 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { BookingEntity } from '../entities';
 import { Repository } from 'typeorm';
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { BookParkingSpotDto } from '../dto/book-parking-spot.dto';
 import { ParkingSpotService } from './parking-spot.service';
 import { UserService } from './user.service';
@@ -35,9 +40,7 @@ export class BookingService implements BookingServiceInterface {
     this.logger.log(`Booking with ID [${id}] deleted successfully.`);
   }
 
-  async bookParkingSpot(
-    bookParkingSpotDto: BookParkingSpotDto,
-  ): Promise<void> {
+  async bookParkingSpot(bookParkingSpotDto: BookParkingSpotDto): Promise<void> {
     const { createdById, startDate, endDate, parkingSpotId } =
       bookParkingSpotDto;
 
@@ -53,6 +56,25 @@ export class BookingService implements BookingServiceInterface {
       );
     }
 
+    const existingBookings = await this.bookingRepository
+      .createQueryBuilder('booking')
+      .where(
+        'booking.parkingSpotId = :parkingSpotId AND ' +
+          '((booking.startDate <= :startDate AND booking.endDate >= :startDate) OR (booking.startDate >= :startDate AND booking.startDate <= :endDate))',
+        {
+          parkingSpotId: bookParkingSpotDto.parkingSpotId,
+          startDate: bookParkingSpotDto.startDate,
+          endDate: bookParkingSpotDto.endDate,
+        },
+      )
+      .getMany();
+
+    if (existingBookings.length > 0) {
+      const errorMessage = `Parking spot: [${bookParkingSpotDto.parkingSpotId}] is already booked for that time: ${bookParkingSpotDto.startDate} - ${bookParkingSpotDto.endDate}.`;
+      this.logger.error(errorMessage);
+      throw new ConflictException(errorMessage);
+    }
+
     const booking = new BookingEntity();
     booking.createdBy = createdBy;
     booking.startDate = startDate;
@@ -61,6 +83,8 @@ export class BookingService implements BookingServiceInterface {
 
     await this.bookingRepository.save(booking);
 
-    this.logger.log(`Booking for user: ${createdBy.firstName} ${createdBy.lastName} created successfully.`);
+    this.logger.log(
+      `Booking for user: ${createdBy.firstName} ${createdBy.lastName} created successfully.`,
+    );
   }
 }
