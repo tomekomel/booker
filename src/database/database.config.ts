5eda9100cca9221fa714f12aa09889f154af4bc7
Tomasz Omeljaniuk <tomekomel@gmail.com>
2024-02-05 13:04:48 +0100

    feat: DB connection fix, add basic postman collection

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,18 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { entitiesList } from './entities-list';
 
 export const dbConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: process.env.DB_HOST ?? 'localhost',
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    autoLoadEntities: true,
-    namingStrategy: new SnakeNamingStrategy(),
-    dropSchema: false,
-    synchronize: false,
-    entitySkipConstructor: true,
+  type: 'postgres',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  dropSchema: false,
+  synchronize: false,
+  entitySkipConstructor: true,
+  entities: entitiesList,
 };
 
 export const loadStrategy = process.env.NODE_ENV !== 'test' ? 'query' : 'join';
